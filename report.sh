1cabf90b79fac239297d0225c993f2b8a7fe7c52
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-26 22:54:25 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+json=~/logs/report-$folder
 source ~/.bash_profile
 
 docker_status=$(docker inspect avs-finalizer-node | jq -r .[].State.Status)
@@ -15,30 +18,23 @@ case $docker_status in
   *) status="error"; message="docker not running" ;;
 esac
 
-# show json output 
-cat << EOF
+cat >$json << EOF
 {
-  "id":"$id",
-  "machine":"$MACHINE",
-  "network":"$network",
-  "chain":"$chain",
-  "type":"node",
-  "status":"$status",
-  "message":"$message",
   "updated":"$(date --utc +%FT%TZ)",
-  "version":"$version"
+  "measurement":"report",
+  "tags": {
+         "id":"$folder",
+         "machine":"$MACHINE",
+         "grp":"node",
+         "owner":"$OWNER"
+  },
+  "fields": {
+        "network":"$network",
+        "chain":"$chain",
+        "status":"$status",
+        "message":"$message",
+        "version":"$version"
+  }
 }
 EOF
-
-# send data to influxdb
-if [ ! -z $INFLUX_HOST ]
-then
- curl --request POST \
- "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$INFLUX_BUCKET&precision=ns" \
-  --header "Authorization: Token $INFLUX_TOKEN" \
-  --header "Content-Type: text/plain; charset=utf-8" \
-  --header "Accept: application/json" \
-  --data-binary "
-    report,id=$id,machine=$MACHINE,grp=$group,owner=$owner status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\",network=\"$network\" $(date +%s%N) 
-    "
-fi
+cat $json
