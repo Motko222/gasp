b5edadd3dbf75dcb7ea6b2bb666db9dfb0c69b50
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-04 21:12:20 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,3 +1,12 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
+cd ~/avs-operator-setup
+docker-compose down
+
+mv /root/avs-operator-setup /root/backup
+mv /root/eigenlayer /root/backup
+mv /root/scripts/$folder
+bash /root/scripts/system/influx-delete.sh $folder
+
+
