3d2a0b078b4dc268f1fea187fbec704a0e4186c3
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-04 21:18:12 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -4,10 +4,10 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd ~/avs-operator-setup
 docker-compose down
 
-mv /root/avs-operator-setup /root/backup
-mv /root/eigenlayer /root/backup
+mv -f /root/avs-operator-setup /root/backup
+mv -f /root/eigenlayer /root/backup
 [ -d /root/backup/scripts ] || mkdir /root/backup/scripts
-mv /root/scripts/$folder /root/backup/scripts
+mv -f /root/scripts/$folder /root/backup/scripts
 bash /root/scripts/system/influx-delete.sh $folder
 
 
